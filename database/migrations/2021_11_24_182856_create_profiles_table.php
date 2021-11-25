9c68057a6ef9993f35a9bc916393e7f0059b0b94
Yualbe Luis <lois21us@gmail.com>
2021-11-25 02:22:03 +0100

    Creando la base de datos mediante la migracion

--- database/migrations/2021_11_24_182856_create_profiles_table.php
+++ database/migrations/2021_11_24_182856_create_profiles_table.php
@@ -15,7 +15,17 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->string('instagram')->nullable();
+            $table->string('github')->nullable();
+            $table->string('web')->nullable();
+            
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
     }
 
