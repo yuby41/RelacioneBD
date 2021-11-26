a6f810194777da1d9a99cab38903f96121740fdb
Yualbe Luis <lois21us@gmail.com>
2021-11-26 13:20:53 +0100

    Todas las tablas: Category, Tag, Taggables, Post y mas

---
+++ database/migrations/2021_11_26_113955_create_taggables_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaggablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('name');
+
+            $table->bigInteger('tag_id')->unsigned();
+
+            $table->morphs('taggable');
+
+            $table->foreign('tag_id')->references('id')->on('tags')
+                 ->onDelete('cascade')
+                 ->onUpdate('cascade');    
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taggables');
+    }
+}
