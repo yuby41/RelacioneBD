45eb2a515287fe11c30b41cd1bd0b87592e0126c
Yualbe Luis <lois21us@gmail.com>
2021-11-29 14:08:24 +0100

    Seeder de Usuarios (Profile, Location, Image & Groups)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,24 @@ class DatabaseSeeder extends Seeder
         factory(App\Level::class)->create(['name' => 'Gold']);
         factory(App\Level::class)->create(['name' => 'Plate']);
         factory(App\Level::class)->create(['name' => 'Bronze']);
+
+        factory(App\User::class, 5)->create()->each(function ($user){
+            $profile = $user->profile()->save(factory(App\Profile::class)->make());
+            $profile->location()->save(factory(App\Location::class)->make());
+            $user->groups()->attach($this->array(rand(1, 3)));
+            $user->image()->save(factory(App\Image::class)->make([
+                'url' => 'https://lorempixel.com/90/90/'
+            ]));
+        });
     }
+
+    public function array($max)
+    {
+        $values = [];
+
+        for($i=1; $i < $max; $i++) {
+            $values[] = $i;
+        }
+    }
+
 }
